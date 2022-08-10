6edb9d4b9f5c993e22663bb1bb6affd2399bddab
MarangoniCow <109583546+MarangoniCow@users.noreply.github.com>
2022-08-10 11:25:09 +0100

    Delete examples/colloid_channel directory

--- examples/colloid_channel/bash_extract.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for (( i=250; i<=2500; i+=250 ))
-do
-tstep=$(printf "%08d" $i)
-#./extract -k vel.001-001.meta vel-${tstep}.001-001
-./extract_colloids config.cds${tstep} 1 col-cdsvel-${tstep}.001-001.csv
-done
