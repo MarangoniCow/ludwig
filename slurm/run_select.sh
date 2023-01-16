f53bc93230dfca41b65658c766d0c8d39f615079
Matthew Sparkes <matthew.sparkes@york.ac.uk>
2023-01-16 13:02:09 +0000

    Updates to run scripts]

--- slurm/run_select.sh
+++ slurm/run_select.sh
@@ -5,16 +5,16 @@ BATCHDIR='/users/mjs572/scratch/ludwig/slurm'
 cwd=$(pwd)
 
 
-NTASKS=20
-MEM=12
+NTASKS=1
+MEM=4
 
 
-inputArr=("./3DP_Pu_Q2D_")
+inputArr=("./ColloidTest01")
 
 
 for i in ${inputArr[@]}; do
 	temp=$( echo $i | awk '{ print substr( $0, 3 ) }')
 	SCRIPTNAME=$(echo "$temp" | cut -f 1 -d '.')
 	echo $SCRIPTNAME
-#	sbatch --ntasks=$NTASKS --ntasks-per-node=$NTASKS --mem=${MEM}gb --job-name=$SCRIPTNAME --time=24:00:00 --output=${BATCHDIR}/logs/%x_J%j --export=SCRIPTNAME=$SCRIPTNAME ${BATCHDIR}/run_multicore.job
+	sbatch --ntasks=$NTASKS --ntasks-per-node=$NTASKS --mem=${MEM}gb --job-name=$SCRIPTNAME --time=00:30:00 --output=${BATCHDIR}/logs/%x_J%j --export=SCRIPTNAME=$SCRIPTNAME ${BATCHDIR}/run_multicore_w_matlab.job
 done
