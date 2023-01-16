aea94fb609b54146af17f7a29f03a6c7330568ea
Matthew Sparkes <matthew.sparkes@york.ac.uk>
2023-01-16 11:48:36 +0000

    Edits to Slurm scripts

--- slurm/run_select.sh
+++ slurm/run_select.sh
@@ -9,12 +9,12 @@ NTASKS=20
 MEM=12
 
 
-inputArr=("./3DC_Pl_AH_01" "./3DC_Pl_AH_05" "./3DC_Pl_AH_07" "./3DC_Pu_AH_03")
+inputArr=("./3DP_Pu_Q2D_")
 
 
 for i in ${inputArr[@]}; do
 	temp=$( echo $i | awk '{ print substr( $0, 3 ) }')
 	SCRIPTNAME=$(echo "$temp" | cut -f 1 -d '.')
 	echo $SCRIPTNAME
-	sbatch --ntasks=$NTASKS --ntasks-per-node=$NTASKS --mem=${MEM}gb --job-name=$SCRIPTNAME --time=24:00:00 --output=${BATCHDIR}/logs/%x_J%j --export=SCRIPTNAME=$SCRIPTNAME ${BATCHDIR}/run_multicore.job
+#	sbatch --ntasks=$NTASKS --ntasks-per-node=$NTASKS --mem=${MEM}gb --job-name=$SCRIPTNAME --time=24:00:00 --output=${BATCHDIR}/logs/%x_J%j --export=SCRIPTNAME=$SCRIPTNAME ${BATCHDIR}/run_multicore.job
 done
