7f42a068c0ff9415b41aa5eeb5e7d7953d958426
Matthew Sparkes <matthew.sparkes@york.ac.uk>
2023-02-27 09:14:16 +0000

    Removed redundant files

--- slurm/run_dir_jobs.sh
+++ slurm/run_dir_jobs.sh
@@ -5,14 +5,14 @@ BATCHDIR='/users/mjs572/scratch/ludwig/slurm'
 cwd=$(pwd)
 
 
-NTASKS=20
-MEM=20
-HRS=12
+NTASKS=10
+MEM=10
+HRS=1
 
 
-for i in ./3DP_Pu_Q2D_H1_*.inp; do
+for i in ./3DP_Pu_Q2D_XL_*.inp; do
 	temp=$( echo $i | awk '{ print substr( $0, 3 ) }')
 	SCRIPTNAME=$(echo "$temp" | cut -f 1 -d '.')
 	echo $SCRIPTNAME
-	sbatch --ntasks=$NTASKS --ntasks-per-node=$NTASKS --mem=${MEM}gb --job-name=$SCRIPTNAME --time=$HRS:00:00 --output=${BATCHDIR}/logs/%x_J%j --export=SCRIPTNAME=$SCRIPTNAME ${BATCHDIR}/run_multicore_w_matlab.job
+	sbatch --ntasks=$NTASKS --ntasks-per-core=$NTASKS --mem=${MEM}gb --job-name=$SCRIPTNAME --time=$HRS:00:00 --output=${BATCHDIR}/logs/%x_J%j --export=SCRIPTNAME=$SCRIPTNAME ${BATCHDIR}/run_multicore_w_matlab.job
 done
